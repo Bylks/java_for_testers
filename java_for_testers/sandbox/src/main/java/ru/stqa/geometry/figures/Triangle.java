ad1daa13c7308a87e90ab8d23f3d0f2645e0fd32
Yakhontov <crz--93@ya.ru>
2025-02-22 11:58:04 +0300

    commit new module

--- java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,7 +1,6 @@
 package ru.stqa.geometry.figures;
 
-public class Triangle {
-double a,b,c;
+public record Triangle (double a, double b,double c) {
     public void printTriangleArea() {
         double area = calculateTriangeArea();
         var text = String.format ("Площадь треугольника со сторонами %.2f , %.2f, %.2f равна %.2f",a,b,c,area);
@@ -10,14 +9,8 @@ double a,b,c;
     public double calculateTriangeArea()
     {
         double p = (a+b+c)/2;
-        double area = Math.sqrt(p*(p-a)*(p-b)*(p-c)); //Считаем площадь
-        return area;
-    }
-   public Triangle (double a1,double b1, double c1){
-        a=a1;//Задаём поля в конструкторе
-        b=b1;
-        c=c1;
-
+        //Считаем площадь
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
     }
     public double calculateTriangePerimeter() {
         return (a+b+c);//Считаем периметр
