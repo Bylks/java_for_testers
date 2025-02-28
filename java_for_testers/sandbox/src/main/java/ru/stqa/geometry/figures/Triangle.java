0b47011712f5c81e7174c76298f01cfee7c109c8
Yakhontov <crz--93@ya.ru>
2025-02-28 13:35:37 +0300

    Задание №4: Потренироваться использовать сравнение объектов

--- java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ java_for_testers/sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,6 +1,9 @@
 package ru.stqa.geometry.figures;
 
-public record Triangle (double a, double b,double c) {
+import java.util.Arrays;
+import java.util.Objects;
+
+public record Triangle (double a, double b, double c) {
     public Triangle
     {
 if (a<=0||b<=0||c<=0)
@@ -10,6 +13,7 @@ if (a<=0||b<=0||c<=0)
     throw new IllegalArgumentException("Created triangle is non-existent");
 }
     }
+
     public void printTriangleArea() {
         double area = calculateTriangleArea();
         var text = String.format ("Площадь треугольника со сторонами %.2f , %.2f, %.2f равна %.2f",a,b,c,area);
@@ -27,5 +31,29 @@ if (a<=0||b<=0||c<=0)
 
     public void printTrianglePerimeter() {
         System.out.println(String.format("Периметр треугольника со сторонами %.2f , %.2f, %.2f равна %.2f",a,b,c, calculateTrianglePerimeter()));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Arrays.equals(this.calculateSortedSides(), triangle.calculateSortedSides()));
+/*             ((Double.compare(a, triangle.a) == 0 && Double.compare(b, triangle.b) == 0 && Double.compare(c, triangle.c) == 0)||
+                (Double.compare(a, triangle.a) == 0 && Double.compare(b, triangle.c) == 0 && Double.compare(c, triangle.b) == 0)||
+                (Double.compare(a, triangle.b) == 0 && Double.compare(b, triangle.c) == 0 && Double.compare(c, triangle.a) == 0)||
+                (Double.compare(a, triangle.b) == 0 && Double.compare(b, triangle.a) == 0 && Double.compare(c, triangle.c) == 0)||
+                (Double.compare(a, triangle.c) == 0 && Double.compare(b, triangle.a) == 0 && Double.compare(c, triangle.b) == 0)||
+                (Double.compare(a, triangle.c) == 0 && Double.compare(b, triangle.b) == 0 && Double.compare(c, triangle.a) == 0)||);//многобуков*/
+    }
+//Возвращает массив отсортированных сторон
+    private double[] calculateSortedSides() {
+        double[] sortedsides = {a,b,c};
+        Arrays.sort(sortedsides);
+        return sortedsides;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 }
--- java_for_testers/sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ java_for_testers/sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -4,64 +4,75 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TriangleTests {
-    Triangle trtest1 = new Triangle(3.0,4.0,5.0);
-    Triangle trtest2 = new Triangle(6.0,8.0,10.0);
-    Triangle trtest3 = new Triangle(10.0,12.0,15.0);
+    Triangle trtest1 = new Triangle(3.0, 4.0, 5.0);
+    Triangle trtest2 = new Triangle(6.0, 8.0, 10.0);
+    Triangle trtest3 = new Triangle(10.0, 12.0, 15.0);
 
     @Test
-    void canCalculateTriangleAreaTest1()
-    {
-        Assertions.assertEquals(6,trtest1.calculateTriangleArea());
+    void canCalculateTriangleAreaTest1() {
+        Assertions.assertEquals(6, trtest1.calculateTriangleArea());
     }
-    @Test
-    void canCalculateTriangleAreaTest2()
-    {
-        Assertions.assertEquals(24,trtest2.calculateTriangleArea());
-    }
-    @Test
-    void canCalculateTriangleAreaTest3()
-    {
-        Assertions.assertEquals(59.81168364124187,trtest3.calculateTriangleArea());
-    }
-    @Test
-    void canCalculateTrianglePerimeterTest1()
-    {
-        Assertions.assertEquals(12,trtest1.calculateTrianglePerimeter());
-    }
-    @Test
-    void canCalculateTrianglePerimeterTest2()
-    {
-        Assertions.assertEquals(24,trtest2.calculateTrianglePerimeter());
-    }
-    @Test
-    void canCalculateTrianglePerimeterTest3()
-    {
-        Assertions.assertEquals(24,trtest3.calculateTrianglePerimeter());
-    }
-    @Test
-    void canCreateTriangleWithNegativeSide ()
-    {
-             try
-             {
-                 new Triangle(-1.0,2.0,3.0);
-                 Assertions.fail();
-             }
-             catch (IllegalArgumentException exception)
-             {//OK
-                  }
 
-    }
     @Test
-    void canCreateNonExistTriangle ()
-    {
-        try
-        {
-            new Triangle(1.0,2.0,13.0);
+    void canCalculateTriangleAreaTest2() {
+        Assertions.assertEquals(24, trtest2.calculateTriangleArea());
+    }
+
+    @Test
+    void canCalculateTriangleAreaTest3() {
+        Assertions.assertEquals(59.81168364124187, trtest3.calculateTriangleArea());
+    }
+
+    @Test
+    void canCalculateTrianglePerimeterTest1() {
+        Assertions.assertEquals(12, trtest1.calculateTrianglePerimeter());
+    }
+
+    @Test
+    void canCalculateTrianglePerimeterTest2() {
+        Assertions.assertEquals(24, trtest2.calculateTrianglePerimeter());
+    }
+
+    @Test
+    void canCalculateTrianglePerimeterTest3() {
+        Assertions.assertEquals(24, trtest3.calculateTrianglePerimeter());
+    }
+
+    @Test
+    void canCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-1.0, 2.0, 3.0);
             Assertions.fail();
-        }
-        catch (IllegalArgumentException exception)
-        {//OK
+        } catch (IllegalArgumentException exception) {//OK
         }
 
+    }
+
+    @Test
+    void canCreateNonExistTriangle() {
+        try {
+            new Triangle(1.0, 2.0, 13.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {//OK
+        }
+
+    }
+
+    @Test
+    void testEqualityNeg() {
+        Assertions.assertNotEquals(trtest1, trtest2);
+    }
+
+    @Test
+    void testEquality1() {
+        var trtestEq1 = new Triangle(3.0, 4.0, 5.0);
+        var trtestEq2 = new Triangle(4.0, 5.0, 3.0);
+        Assertions.assertEquals(trtestEq1, trtestEq2);
+    }
+    @Test
+    void testEquality2() {
+        var trtestEq1 = new Triangle(5.0, 4.0, 3.0);
+        var trtestEq2 = new Triangle(4.0, 3.0, 5.0);
+        Assertions.assertTrue(trtestEq1.equals(trtestEq2));
     }
 }
