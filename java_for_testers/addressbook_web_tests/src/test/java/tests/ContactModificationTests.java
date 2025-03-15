84924b889a21e3bbd74d6bfc416f580e2c5833bd
Yakhontov <crz--93@ya.ru>
2025-03-15 14:54:39 +0300

    Задание №11: Сделать тесты для модификации групп и модификации контактов

--- java_for_testers/addressbook_web_tests/src/test/java/tests/ContactModificationTests.java
+++ java_for_testers/addressbook_web_tests/src/test/java/tests/ContactModificationTests.java
@@ -23,7 +23,7 @@ public class ContactModificationTests extends TestBase {
         app.contacts().editContact(oldContacts.get(index), editedContact);
         var newContacts = app.contacts().getList();
         var expectedList = new ArrayList<>(oldContacts);
-        expectedList.set(index, editedContact.withChangedId(oldContacts.get(index).id())); // убираем из проверки все кроме Id и имени т.к. со страницы из getList() можем получить только это
+        expectedList.set(index, editedContact.withChangedId(oldContacts.get(index).id())); //  убираем из проверки все кроме Id и имени т.к. со страницы из getList() можем получить только это
         Comparator<ContactData> compareById = (o1, o2) ->
        {return Integer.compare((Integer.parseInt(o1.id())), Integer.parseInt(o2.id()));};
         newContacts.sort(compareById);
